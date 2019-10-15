a08f97c8303cb947ac04c14926480329c2f5f2fd
Arindam Biswas <arindam.biswas@gmail.com>
2019-10-15 17:03:16 +0530

    - initial commit

---
+++ render.js
@@ -0,0 +1,68 @@
+var system = require('system');
+var page = require('webpage').create();
+var WAIT_FOR_RENDER_INTERVAL = 5000;
+var PROCESS_TIMEOUT = 8000;
+
+page.onConsoleMessage = function (msg, lineNum, sourceId) {
+    console.log('CONSOLE: ' + msg + ' (from line #' + lineNum + ' in "' + sourceId + '")');
+
+    if (msg === 'Google.chart.ready') {
+        window.ischartready = true;
+    }
+};
+
+var chartData;
+
+page.open('chartData.json', function () {
+
+    var jsonSource = page.plainText;
+    chartData = JSON.parse(jsonSource);
+    console.log('json loaded', chartData.data);
+
+    page.open('chart.html', function() {
+
+        page.evaluate(function (chartData) {
+
+            console.log('Evaluate : ', chartData);
+            function chartReadyCallback() {
+                console.log('Google.chart.ready');
+            }
+
+            function drawRegionsMap() {
+                var data = google.visualization.arrayToDataTable(chartData.data);
+                var options = {
+                    width: 400,
+                    height: 200
+                };
+                var chart = new google.visualization
+                    .GeoChart(document.getElementById('chart_div'));
+                chart.draw(data, options);
+                google.visualization.events.addListener(chart, 'ready', chartReadyCallback);
+            };
+
+            drawRegionsMap();
+        }, chartData);
+    
+        timeoutTime = PROCESS_TIMEOUT;
+        interval = window.setInterval(function () {
+            console.log('waiting');
+            if (window.ischartready) {
+                clearTimeout(timer);
+                clearInterval(interval);
+                page.paperSize = { format: 'A4', orientation: 'landscape' };
+                page.render(system.args[1]);
+                phantom.exit();    
+            }
+        }, 50);
+    
+        // we have a timeoutTime second timeframe..
+        timer = window.setTimeout(function () {
+            clearInterval(interval);
+            exitCallback('ERROR: While rendering, there\'s is a timeout reached');
+            phantom.exit();
+        }, timeoutTime);
+    
+    });
+});
+
+
